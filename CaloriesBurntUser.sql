11be0b151f3482e3bf68ef193bf2885de1d5ac7d
nrthokala <89919020+nrthokala@users.noreply.github.com>
2024-04-23 17:15:49 -0400

    Delete CaloriesBurntUser.sql

--- CaloriesBurntUser.sql
+++
@@ -1,7 +0,0 @@
-SELECT EL.UserId, SUM(E.caloriesPerKg * UG.currentWeight) AS CaloriesBurnt, EL.ExerciseLogDate
-FROM “NeoFitness”.”ExerciseLog” AS EL
-JOIN  “NeoFitness”.”Exercise” AS E ON E.ExerciseId = EL.ExerciseId
-JOIN  “NeoFitness”.”UserGoal” AS UG ON UG.UserId = EL.UserId 
-WHERE EXTRACT(MONTH FROM EL.ExerciseLogDate) = 4 
-AND EXTRACT(YEAR FROM EL.ExerciseLogDate) = 2024 
-GROUP BY EL.UserId, EL.ExerciseLogDate;
